992d4f07cca435f059f7ef797699eb43af9aa6a7
tomas1433 <t.tourlet@gmail.com>
2016-09-22 16:27:43 +0000

    add seller pages + link to profil

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,14 @@
 class ServicesController < ApplicationController
  skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :find_service, only: [:show, :edit, :update]
-
+before_filter :check_user, only: [:edit, :update]
+  
+  def seller
+    @services = Service.where(user: current_user)
+  end
+  
+  
+  
   def index
     if params[:category]
       @services = Service.where(category: params[:category])
@@ -50,4 +57,9 @@ class ServicesController < ApplicationController
     @service = Service.find(params[:id])
   end
 
+  def check_user
+    if current_user != @service.user
+      redirect_to root_url, alert: "No puede realizar esta accion"
+    end
+  end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -5,7 +5,7 @@ class Service < ApplicationRecord
   validates :user, presence: true
   validates :name, presence: true
   validates :city, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0}
   validates :category, presence:true, inclusion: { in: %w(Check-in Check-out Limpieza), message: "%{value} no es una entrada correcta" }
   #validates :rating, inclusion: {in: [0, 1, 2, 3, 4, 5]}
 end
