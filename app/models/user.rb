f725ca90ae78c26db4360e3fff97899089d68978
tomas1433 <t.tourlet@gmail.com>
2016-09-23 15:11:22 +0000

    add phone number to user model and avatar to signin form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :services
   has_attachment :avatar
+  has_attachment :phone
   has_many :sales, class_name: "Request", foreign_key: "seller_id"
   has_many :purchases, class_name: "Request", foreign_key: "buyer_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :phone, presence: true
+
 end
